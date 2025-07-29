94029b5897618f883fd5a48ca1c3ad57654f29c7
Michal Rogozinski <mjrogozinski@gmail.com>
2025-07-29 21:39:22 +0200

    kanagawa color scheme replaces tokyonight

---
+++ lua/kickstart/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      vim.cmd.colorscheme 'kanagawa'
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
