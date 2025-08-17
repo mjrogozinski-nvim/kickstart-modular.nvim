f789a46856bd36173353e74398b634dc6bef93dd
Michal Rogozinski <mjrogozinski@gmail.com>
2025-08-17 10:19:38 +0200

    adding mini.surround fix

--- lua/plugins/mini/surround.lua
+++ lua/plugins/mini/surround.lua
@@ -1,4 +1,16 @@
 -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 -- - sd'   - [S]urround [D]elete [']quotes
 -- - sr)'  - [S]urround [R]eplace [)] [']
-return { 'echasnovski/mini.surround', opts = {}, version = false }
+return {
+  'echasnovski/mini.surround',
+  opts = {},
+  version = false,
+  config = function()
+    -- https://github.com/echasnovski/mini.nvim/blob/main/doc/mini-surround.txt#L573C3-L573C45
+    --
+    -- Note: if 'timeoutlen' is low enough to cause occasional usage of |s| key
+    -- (that deletes character under cursor), disable it with the following call: >lua
+    --
+    vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
+  end,
+}
