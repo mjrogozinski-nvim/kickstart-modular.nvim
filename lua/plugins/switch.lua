41dec43f2c55eea93e9c6a7969b0d18c8adf04c6
Michal Rogozinski <mjrogozinski@gmail.com>
2025-08-28 20:44:05 +0200

    introducing switch and repurposing <leader>ss + <leader>SS for Telescope's SS

---
+++ lua/plugins/switch.lua
@@ -0,0 +1,12 @@
+return {
+  'AndrewRadev/switch.vim',
+  keys = {
+    {
+      '<leader>ss',
+      ':Switch<CR>',
+      mode = '',
+      desc = ':Switch',
+      silent = true,
+    },
+  },
+}
