a3c8eb18c901a7b270c182c445f27bc0c7201787
Michal Rogozinski <mjrogozinski@gmail.com>
2025-10-25 09:57:13 +0200

    the screen was flickering in case of errors

--- lua/code/utils/save-writable-buffers.lua
+++ lua/code/utils/save-writable-buffers.lua
@@ -6,9 +6,6 @@ function Save_all_writable_buffers()
           vim.cmd 'silent! write'
         end)
       end)
-      if not ok then
-        vim.notify('Failed to save buffer ' .. buf .. ': ' .. err, vim.log.levels.WARN)
-      end
     end
   end
 end
