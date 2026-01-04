72a0556108b980ba78fce91677e638927dd1686a
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-04 19:03:26 +0100

    todo

--- lua/code/utils/save-writable-buffers.lua
+++ lua/code/utils/save-writable-buffers.lua
@@ -1,3 +1,4 @@
+-- TODO: prevent asking to save open e.g. STL headers
 function Save_all_writable_buffers()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'modifiable') then
