d76a841cc6fdb3ce77c2fe9d43e988954b62219d
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-06 13:20:35 +0100

    moving lua debuggin stuff to ftplugin

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -6,3 +6,16 @@ vim.keymap.set('n', '<leader>X', ':lua require"osv".run_this()<cr>', in_current_
 vim.keymap.set('n', '<leader>dl', function()
   require('osv').launch { port = 8086 }
 end, in_current_buffer.with_desc ' Launch Lua debug adapter')
+
+local dap = require 'dap'
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = 'Attach to running Neovim instance',
+  },
+}
+
+dap.adapters.nlua = function(callback, config)
+  callback { type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086 }
+end
