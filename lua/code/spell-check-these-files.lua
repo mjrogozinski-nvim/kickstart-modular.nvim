7fef57adc1a485dfc49562f6f78622102c77d66e
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-06 13:00:18 +0100

    removing superfluous comment

--- lua/code/spell-check-these-files.lua
+++ lua/code/spell-check-these-files.lua
@@ -1,7 +1,6 @@
 -- Enable spell checking for certain file types
 vim.api.nvim_create_autocmd(
   { 'BufRead', 'BufNewFile' },
-  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
   {
     pattern = { '*.txt', '*.md', '*.tex', 'gitcommit' },
     callback = function()
