a24cfb1b70a9e6c91ce117355e518e5b550bee0c
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-06 13:05:46 +0100

    adding comment

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,2 +1,5 @@
+-- spell check git commit messages
+-- git commit messages are of language gitcommit
+-- somehow the autocommand from spell-check-this-file did not work
 vim.opt.spell = true
 vim.opt.spelllang = 'en'
