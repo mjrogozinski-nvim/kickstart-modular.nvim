a96a8cef0cf471325ae676d27153ea261b64f6c7
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-07 13:30:13 +0100

    q will close :Git diff

--- lua/code/sane/q-closes-help.lua
+++ lua/code/sane/q-closes-help.lua
@@ -2,5 +2,6 @@ local q_quits_filetypes = {
   'man', -- :Man foo
   'help', -- :h foo
   'fugitive', -- :Git
+  'git', -- :Git diff
 }
 vim.api.nvim_create_autocmd('FileType', { pattern = q_quits_filetypes, command = [[nnoremap <buffer><silent> q :quit<CR>]] })
