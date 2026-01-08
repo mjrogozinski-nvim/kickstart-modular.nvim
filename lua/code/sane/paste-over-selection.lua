5c862f360ec24bd271e1bdceaca6c8c3d0817dd8
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-08 21:01:20 +0100

    renaming paste over selection, as it's in visual mode only

--- lua/code/sane/paste-over-selection.lua
+++
@@ -1,2 +0,0 @@
--- Paste over selection without yanking
-vim.keymap.set("v", "p", '"_dP', { desc = "Paste (no yank)" })
---
+++ lua/code/sane/visual-paste-over-selection.lua
@@ -0,0 +1,2 @@
+-- Paste over selection without yanking
+vim.keymap.set('v', 'p', '"_dP', { desc = 'Paste (no yank)' })
