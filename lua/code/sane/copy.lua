3af633290ca629320e9705c441a7853193911378
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-09 21:44:33 +0100

    <leader>Y sane copy

--- lua/code/sane/copy.lua
+++ lua/code/sane/copy.lua
@@ -1,1 +1,2 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'copy to system clipboard (+ register)' })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', { desc = 'copy to system clipboard (+ register)' })
