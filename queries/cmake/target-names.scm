19537a6f1f781a21de1ba3a85d3fbd4deff15734
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-14 19:13:14 +0100

    replacing regex with duplication
    there are no facilities in ts that make reuse possible

--- queries/cmake/target-names.scm
+++ queries/cmake/target-names.scm
@@ -1,7 +1,19 @@
 ((normal_command (identifier) @function.name
-                (argument_list
+                 (argument_list
+                  .
                   (argument
                     (unquoted_argument) @target.name)
+                  .
                   (_)))
-  (#match? @function.name "^add_executable|add_library$")
+  (#eq? @function.name "add_executable")
+)
+
+((normal_command (identifier) @function.name
+                 (argument_list
+                  .
+                  (argument
+                    (unquoted_argument) @target.name)
+                  .
+                  (_)))
+  (#eq? @function.name "add_library")
 )
