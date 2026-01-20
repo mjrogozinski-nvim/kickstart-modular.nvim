3f5b608542dc13cf97058c3ae4fc55b904f8b79f
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-20 05:48:37 +0100

    removing pl spelling
    as dicts are not available everywhere

--- lua/code/spell-check-these-files.lua
+++ lua/code/spell-check-these-files.lua
@@ -1,11 +1,8 @@
 -- Enable spell checking for certain file types
-vim.api.nvim_create_autocmd(
-  { 'BufRead', 'BufNewFile' },
-  {
-    pattern = { '*.txt', '*.md', '*.tex', 'gitcommit' },
-    callback = function()
-      vim.opt.spell = true
-      vim.opt.spelllang = 'en,pl'
-    end,
-  }
-)
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = { '*.txt', '*.md', '*.tex', 'gitcommit' },
+  callback = function()
+    vim.opt.spell = true
+    vim.opt.spelllang = 'en'
+  end,
+})
