a1ec02be3abba0d09d7c8a3f53504a7a0bef713d
Michal Rogozinski <mjrogozinski@gmail.com>
2026-01-20 05:50:27 +0100

    removing leader>q to open quickfix list
    it was replaced by leader>xQ from trouble

--- lua/code/qflist-open-close.lua
+++ lua/code/qflist-open-close.lua
@@ -1,3 +1,1 @@
 vim.keymap.set('n', '<leader>cl', ':cclose<CR>', { silent = true, desc = '[C][l]ose quickfix list' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
--- TODO: ? <leader>q for closing, now it's <leader>cl
