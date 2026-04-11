7182a0f98d82e402f255fe9925959ef09b5cca3c
Michal Rogozinski <mjrogozinski@gmail.com>
2026-04-11 09:15:57 +0200

    q closes quickfix

--- lua/code/sane/q-closes-buffers.lua
+++ lua/code/sane/q-closes-buffers.lua
@@ -5,6 +5,7 @@ local q_quits_filetypes = {
   'git', -- :Git diff
   'gitsigns-blame', -- :Gitsigns blame, leader hB
   'OverseerOutput', -- Ctrl+S in leader co
-  'neotest-summary' -- <leader>io
+  'neotest-summary', -- <leader>io
+  'qf' -- quickfix list
 }
 vim.api.nvim_create_autocmd('FileType', { pattern = q_quits_filetypes, command = [[nnoremap <buffer><silent> q :quit<CR>]] })
